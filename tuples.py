589b3b78e2a3fcf38ebaf261684cb16a690e528d
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-01-03 11:33:51 -0300

    072 exercise

--- tuples.py
+++ tuples.py
@@ -1,4 +1,13 @@
 # obs: tuples are immutable
 # structure:
 lunch=('hamburguer','juice','pizza','pudim')
-print('rebase')
+
+#072
+tuple=('one','two','three','four','five','six','seven','eight','nine','ten')
+user=int(input('type a number between 0 and 10: '))
+i=0
+while True:
+    while user<0 or user>10:
+        user=int(input('PLEASE, TRY AGAIN! type a number between 0 and 10: '))
+    print('\n-- You have typed zero...' if user==0 else f'-- You have typed {tuple[user-1]}...')
+    break
