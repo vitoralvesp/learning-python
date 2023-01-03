bc8d88c6e5c8193149c85b8f78dbdac98d92b2e0
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-01-03 16:26:18 -0300

    073 exercise

--- tuples.py
+++ tuples.py
@@ -2,7 +2,7 @@
 # structure:
 lunch=('hamburguer','juice','pizza','pudim')
 
-#072
+'''#072
 tuple=('one','two','three','four','five','six','seven','eight','nine','ten')
 user=int(input('type a number between 0 and 10: '))
 i=0
@@ -11,3 +11,11 @@ while True:
         user=int(input('PLEASE, TRY AGAIN! type a number between 0 and 10: '))
     print('\n-- You have typed zero...' if user==0 else f'-- You have typed {tuple[user-1]}...')
     break
+'''
+#073
+futeboltuple=('Palmeiras','Internacional','Fluminense','Corinthians','Flamengo','Atlético Paranaense','Atlético Mineiro','Fortaleza','São Paulo','América Fc Saf','Chapecoense')
+firstplaced=futeboltuple[0:5]
+lastplaced=futeboltuple[-4:]
+alphabeticalorder=sorted(futeboltuple)
+chapecoense=futeboltuple.index('Chapecoense')
+print(f'-- First Placed: {firstplaced}\n-- Last Placed: {lastplaced}\n-- Alphabetical Order: {alphabeticalorder}\n-- Chapecoense Position: {chapecoense+1}')
