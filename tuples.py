043e22ab4d6ea524369013a3f06f3c68dc8324ca
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-01-05 18:26:53 -0300

    076 exercise

--- tuples.py
+++ tuples.py
@@ -28,7 +28,7 @@ for i in range(0,5):
     tuple=(randomnumber,)
     finaltuple+=tuple
 print(f'\n-- finaltuple: {finaltuple}\n-- biggestvalue and lowestvalue: {max(finaltuple)}, {min(finaltuple)}')
-'''
+
 #075
 finaltuple=evennumbers=()
 print('\n')
@@ -42,3 +42,32 @@ print(f'\n-- finaltuple: {finaltuple}')
 print(f'\n-- how many times does 9 appear: {finaltuple.count(9)}')
 print(f'\n-- index of the first 3 entered: {finaltuple.index(3)}' if 3 in finaltuple else '\n-- number 3 was not entered')
 print(f'\n-- even numbers: {evennumbers}')
+'''
+#076
+productstuple=()
+addmore='Y'
+i=0
+while True:
+    while addmore!='N':
+        productname=str(input("\n1. enter the product's name: ")).upper()
+        while productname=='':
+            print('\n**TRY AGAIN WITH A VALID OPTION**')
+            productname=str(input("1. enter the product's name: ")).upper()
+        productvalue=float(input("2. enter the product's value: R$"))
+        addmore=str(input('3. do you want to add more [Y/N]: ')).upper().strip()[0]
+        tuple=(productname,productvalue,)
+        productstuple+=tuple
+    print('\n\n')
+    print('-'*40)
+    print(f'{"PRODUCTS LIST":^40}')
+    print('-'*40)
+    for i in range(0,len(productstuple)):
+        stringvalue=type(productstuple[i])==str
+        floatvalue=type(productstuple[i])==float
+        if stringvalue:
+            print(f'{productstuple[i]:.<30}',end='')
+        else:
+            if floatvalue:
+                print(f'R${productstuple[i]:>7.2f}')
+    print('-'*40)
+    break
