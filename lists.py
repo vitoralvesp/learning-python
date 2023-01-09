2ae20e821c1143e4c39e235b3c35b8e9360283b3
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-01-09 18:47:09 -0300

    078 exercise

--- lists.py
+++ lists.py
@@ -12,3 +12,18 @@ alphabet.sort() #puts in order (strings, integers, ...)
 newlunch=lunch[:] # copy lunch without conect them
 newlunch[2]='orange juice' # change newlunch (lunch not affected)
 
+#078
+numbers=list()
+highernumber=lowernumber=0
+for i in range(0,4):
+    numbers.append(int(input(f"{i+1}. type a random number: ")))
+    if i==0:
+        highernumber=lowernumber=numbers[i]
+    else:
+        if numbers[i]>highernumber:
+            highernumber=numbers[i]
+        if numbers[i]<lowernumber:
+            lowernumber=numbers[i]
+print(f'\n> List: {numbers}\n> Higher Number: {highernumber} (INDEX {numbers.index(highernumber)})\n> Lower Number: {lowernumber} (INDEX {numbers.index(lowernumber)})')
+
+    
