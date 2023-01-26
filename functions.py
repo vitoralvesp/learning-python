20d13b85c37b1c744eb56d507e798c0605c578a3
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-01-26 16:10:29 -0300

    098 exercise

--- functions.py
+++ functions.py
@@ -14,7 +14,7 @@ def addNumbers(*num):
     print(total)
 addNumbers(1,2,3,4,5) #15  
 addNumbers(1,8) #9
-'''
+
 #096
 def area(largura,comprimento):
     area=largura*comprimento
@@ -24,8 +24,44 @@ largura=float(input('Largura (m): '))
 comprimento=float(input('Comprimento (m): '))
 area(largura,comprimento)
 
+#097
+def escreva(texto):
+    print('~'*40)
+    print(f'{texto:^40}')
+    print('~'*40)
+texto=str(input('> Insira sua mensagem aqui: '))
+escreva(texto)
+'''
+#098
+from time import sleep
+def contador(inicio,fim,passo):
+    if passo<0:
+        passo*=-1
+    if passo==0:
+        passo=1
+    print('-'*40)
+    print(f'Contagem de {inicio} à {fim} de {passo} em {passo}:')
+    if inicio<fim:
+        i=inicio
+        while i<fim+1:
+            print(f'{i} ',end='',flush=True)
+            sleep(.5)
+            i+=passo
+        print('FIM!')
+    else:
+        i=inicio
+        while i>=fim:
+            print(f'{i} ',end='',flush=True)
+            sleep(.5)
+            i-=passo
+        print('FIM!')
 
+contador(1,10,1)
+contador(10,0,2)
 
-
-
-
+print('-'*40)
+print(f'Agora é a sua vez de personalizar a contagem!')
+inicio=int(input('Início: '))
+fim=int(input('Fim: '))
+passo=int(input('Passo: '))
+contador(inicio,fim,passo)
